0f3530c7a16451fc39a9c9109b5fc3b6b25342bc
Wayne Schmidt <wschmidt@sumologic.com>
2021-03-05 14:53:32 +0900

    initial commit

---
+++ bin/dropbox-download.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+"""
+Simple script to retrieve Dropbox data
+"""
+
+import os
+import sys
+import json
+import datetime
+import logging
+import requests
+
+logging.basicConfig(level=logging.INFO)
+
+DROPBOX_BASE_DIR = '/var/lock/dropbox'
+
+DROPBOX_LOCK_DIR = os.path.join(DROPBOX_BASE_DIR, 'lock')
+DROPBOX_LOCK_FILE = os.path.join(DROPBOX_LOCK_DIR, 'dropbox-timestamp.lock')
+logging.info('lock_directory: %s', DROPBOX_LOCK_FILE)
+
+DROPBOX_LOGS_DIR = os.path.join(DROPBOX_BASE_DIR, 'logs')
+DROPBOX_LOGS_FILE = os.path.join(DROPBOX_LOGS_DIR, 'dropbox-timestamp.log')
+logging.info('logs_directory: %s', DROPBOX_LOGS_FILE)
+
+TODAY = datetime.datetime.today()
+
+TIMESTAMP = "{:04d}-{:02d}-{:02d}T00:00:00.000".format(TODAY.year, TODAY.month, TODAY.day)
+
+DROPBOX_BASE_URL = 'https://api.dropboxapi.com/2/team_log/get_events'
+
+def get_timestamp_data():
+    """
+    Collect the timestamp from either a local file or current date: DROPBOX_LOCK_FILE
+    """
+
+    for target_dir in ( DROPBOX_LOCK_DIR, DROPBOX_LOGS_DIR ):
+        if not os.path.exists(target_dir):
+            logging.info('creating_directory: %s', target_dir)
+            os.makedirs(target_dir)
+
+    my_timestamp = TIMESTAMP
+
+    if os.path.exists(DROPBOX_LOCK_FILE):
+        logging.info('reading_timestamp: %s', DROPBOX_LOCK_FILE)
+        with open(DROPBOX_LOCK_FILE, 'r') as time_stamp_file:
+            my_timestamp = time_stamp_file.read()
+
+    return my_timestamp + 'Z', DROPBOX_LOCK_FILE
+
+def put_timestamp_data(my_tsvalue, my_tsfile):
+    """
+    Persist the timestamp into a local file for comparison: DROPBOX_LOCK_FILE
+    """
+
+    my_timestamp = my_tsvalue.replace(microsecond=0).isoformat()
+    try:
+        with open(my_tsfile, 'w') as time_stamp_file:
+            logging.info('writing_timestamp: %s', DROPBOX_LOCK_FILE)
+            time_stamp_file.write(my_timestamp)
+    except OSError as my_error:
+        logging.info('Unexpected error (%d): %s', my_error.errno, my_error.strerror)
+        raise
+    return my_timestamp + 'Z'
+
+def remove_dot_key(json_object):
+    """
+    Clean up Json object: json_object
+    """
+
+    for key in json_object.keys():
+        new_key = json_object.replace(".","")
+        if new_key != key:
+            json_object[new_key] = json_object[key]
+            del json_object[key]
+    return json_object
+
+if __name__ == '__main__':
+
+    timestamp, dropboxLockFile = get_timestamp_data()
+
+    now = put_timestamp_data(datetime.datetime.now(), dropboxLockFile)
+
+    #  --data "{\"time\": { \"start_time\": \"2018-4-13T14:00:00Z\" }}"
+
+    start_end_time = { 'start_time': timestamp, 'end_time': now }
+    jsonData= { 'time': start_end_time }
+
+    DROPBOX_TARGET_URL = DROPBOX_BASE_URL
+
+    TOTAL_EVENTS = 0
+
+    CONTINUE_FLAG = "no"
+
+    GET_DATA = "true"
+
+    while GET_DATA == "true":
+
+        header_dict = {}
+
+        header_dict['Accept'] = 'application/json'
+        header_dict['Content-Type'] = 'application/json'
+        header_dict['Authorization'] = 'Bearer <Enter your access token here>'
+
+        get_response = requests.get(DROPBOX_TARGET_URL,headers=header_dict)
+        my_status = get_response.status_code
+        my_payload = get_response.content
+
+        if get_response.status_code != 200:
+            logging.error('status_code: %d - %s', my_status, my_payload)
+            sys.exit(get_response.status_code)
+        else:
+            dropbox_json_logs = json.loads(get_response.content, object_hook=remove_dot_key)
+        events = dropbox_json_logs['events']
+
+        events_size = len(events)
+        TOTAL_EVENTS += events_size
+
+        output_file = open(DROPBOX_LOGS_FILE, 'a+')
+        for event in events:
+            jsonLog = json.dumps(event)
+            output_file.write('{}\n'.format(jsonLog))
+
+        if dropbox_json_logs['has_more'] == 'true':
+            DROPBOX_TARGET_URL = 'https://api.dropboxapi.com/2/team_log/get_events/continue'
+            jsonData = { 'cursor': dropbox_json_logs['cursor'] }
+            logging.info('Retrieved: %d bytes and getting more data.', events_size)
+        else:
+            GET_DATA = "false"
---
+++ readme.md
@@ -0,0 +1,74 @@
+Dropbox Downloader
+==================
+
+This is a basic download wrapper for dropbox logs
+
+Installing the Scripts
+=======================
+
+The scripts are command line based, designed to be used within a batch script or DevOPs tool such as Chef or Ansible.
+Each script is a python3 script, and the complete list of the python modules will be provided to aid people using a pip install.
+
+You will need to use Python 3.6 or higher and the modules listed in the dependency section.  
+
+The steps are as follows: 
+
+    1. Download and install python 3.6 or higher from python.org. Append python3 to the LIB and PATH env.
+
+    2. Download and install git for your platform if you don't already have it installed.
+       It can be downloaded from https://git-scm.com/downloads
+    
+    3. Open a new shell/command prompt. It must be new since only a new shell will include the new python 
+       path that was created in step 1. Cd to the folder where you want to install the scripts.
+    
+    4. Execute the following command to install pipenv, which will manage all of the library dependencies:
+    
+        sudo -H pip3 install pipenv 
+ 
+    5. Clone this repository. This will create a new folder
+    
+    6. Change into this folder. Type the following to install all the package dependencies 
+       (this may take a while as this will download all of the libraries it uses):
+
+        pipenv install
+        
+Dependencies
+============
+
+See the contents of "pipfile"
+
+Script Names and Purposes
+=========================
+
+    1. ./bin - has all of the scripts
+          ./bin/dropbox-download.py
+
+To Do List:
+===========
+
+* Add options to customize script analysis
+
+License
+=======
+
+Copyright 2021 Wayne Kirk Schmidt
+
+Licensed under the GNU GPL License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    license-name   GNU GPL
+    license-url    http://www.gnu.org/licenses/gpl.html
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Support
+=======
+
+Feel free to e-mail me with issues to: wayne.kirk.schmidt@gmail.com
+I will provide "best effort" fixes and extend the scripts.
+
